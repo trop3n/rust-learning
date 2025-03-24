1821edc1a39edf190bd681f91427ec57d5b7b093
trop3n <jasondkimm@proton.me>
2025-03-23 21:05:16 -0700

    working on guessing game in ch02

--- rust-books/rust-book/guessing_game/src/main.rs
+++ rust-books/rust-book/guessing_game/src/main.rs
@@ -5,6 +5,8 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
+    println!("The secret number is: {secret_number}");
+
     println!("Please input your guess.")
     let mut guess = String::new();
     io:stdin()
